9315fb2de3fba28e242d7d448acfe1311be9feba
Viennarz Curtiz <viennarzc@gmail.com>
2022-02-05 08:40:33 +0800

    Initial Commit

---
+++ Kainsh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kainsh
+//
+//  Created by Viennarz Curtiz on 2/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kainsh/KainshApp.swift
@@ -0,0 +1,17 @@
+//
+//  KainshApp.swift
+//  Kainsh
+//
+//  Created by Viennarz Curtiz on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct KainshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
